f8bd49fb4bfe18272bffb1d042670a6ed96a07bd
Juandoqg <juandoqg@gmail.com>
2025-07-06 17:08:15 -0500

    dependencias de microservices para user-service

--- auth-service/src/auth/auth.controller.ts
+++ auth-service/src/auth/auth.controller.ts
@@ -6,13 +6,11 @@ import { Controller, Post, Body } from '@nestjs/common';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // Ruta HTTP para login desde Postman
   @Post('login')
   async loginHttp(@Body() loginData: { username: string; password: string }) {
     return this.authService.loginHttp(loginData.username, loginData.password);
   }
 
-  // Este es para comunicación entre microservicios (déjalo si lo vas a necesitar)
   @MessagePattern({ cmd: 'login' })
   async loginMicroservice(data: { user: any; password: string }) {
     const user = await this.authService.validateUser(data.user, data.password);
@@ -22,7 +20,6 @@ export class AuthController {
     return { error: 'Invalid credentials' };
   }
 
-  // Ruta HTTP para registrar usuarios desde Postman
   @Post('register')
   async register(@Body() userData: { username: string; password: string }) {
     return this.authService.createUser(userData);
--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  // Levantar HTTP
   const app = await NestFactory.create(AppModule);
 
   // Conectar microservicio TCP
